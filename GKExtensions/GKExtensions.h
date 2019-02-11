53c4aa3428d70a50228cd01a1a95dfd014b5ba87
Kirill Opekishev <grumpykir@gmail.com>
2019-02-11 16:26:19 +0300

    Initial Commit

---
+++ GKExtensions/GKExtensions.h
@@ -0,0 +1,19 @@
+//
+//  GKExtensions.h
+//  GKExtensions
+//
+//  Created by  Кирилл on 2/11/19.
+//  Copyright © 2019 AppCraft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GKExtensions.
+FOUNDATION_EXPORT double GKExtensionsVersionNumber;
+
+//! Project version string for GKExtensions.
+FOUNDATION_EXPORT const unsigned char GKExtensionsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GKExtensions/PublicHeader.h>
+
+
